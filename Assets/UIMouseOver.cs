97691348e181e2dfa6d1720e3e0223fb0d0950d1
FuDaVan <fudavan@yahoo.de>
2022-10-21 15:05:35 +0200

    WTF? 330 changes? - UI buttons added
    - skript zum würfel moven in arbeit
    - todo: nice button sprites/images machen

--- Assets/UIMouseOver.cs
+++ Assets/UIMouseOver.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIRotateDie : MonoBehaviour
+public class UIMouseOver : MonoBehaviour
 {
-    Vector3 rotVec = new Vector3(5, 10, 0);
-    public float rotationSpeed = 5;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +13,6 @@ public class UIRotateDie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotVec * rotationSpeed * Time.deltaTime);
+        
     }
 }
