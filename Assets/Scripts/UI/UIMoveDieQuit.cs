c1d9bf46b01e18376686f1137524afa74baa1895
FuDaVan <fudavan@yahoo.de>
2022-10-23 14:11:58 +0200

    Die Main Menu
    Der Die bewegt sich jetzt im Main Menu je nachdem, auf welchen button der mauszeiger zeigt.
    pro button ein script. verbesserung?

---
+++ Assets/Scripts/UI/UIMoveDieQuit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMoveDieQuit : MonoBehaviour, IPointerEnterHandler
+{
+    Vector3 posQuit = new Vector3((float)-8.25, (float)-2.5, 0);
+
+    // When pointer enters quit button area
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GameObject.FindGameObjectWithTag("Die").transform.position = posQuit;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
