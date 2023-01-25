f01d4e64835335b8e823084eb5ae3b51dca691ae
Mauldren <116089587+Mauldren@users.noreply.github.com>
2023-01-25 20:02:18 +0100

    Deck Library und Frefabs erstellt

--- Assets/Scripts/Deck/DeckLibrary.cs
+++ Assets/Scripts/Deck/DeckLibrary.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DeckLibrary : MonoBehaviour
 {
     public Deck activeDeck;
 
+    public Transform dieCardPrefab;
+
+    public Transform dieListObjPrefab;
+
+    public Transform diceCardsParent;
+
+    public Die[] arrayMitDice = new Die[3] { new Die_Normal_Default_D6(0), new Die_Normal_Highroll_D6(0), new Die_Normal_Midroll_D4(0) };
+
+    public Transform[] dieCards = new Transform[3];
+
 
     void Start()
     {
-        
+        int distance = 0;
+        for (int i = 0; i < arrayMitDice.Length; i++)
+        {
+            dieCards[i] = Instantiate(dieCardPrefab, diceCardsParent.transform.position, Quaternion.identity, diceCardsParent);
+            dieCards[i].position = new Vector3(260 + distance, 380, 0);
+            dieCards[i].GetChild(0).GetComponent<TextMeshProUGUI>().text = arrayMitDice[i].dieName;
+            dieCards[i].GetChild(1).GetComponent<TextMeshProUGUI>().text = arrayMitDice[i].description;
+            dieCards[i].GetComponent<Button>().onClick.AddListener(delegate { addDice(arrayMitDice[i]); });
+            distance += 175;
+        }
     }
 
 
     void Update()
     {
-        
+
     }
+    public void addDice(Die die)
+    {
+        activeDeck.addDie(die);
+        Debug.Log(die.dieName);
+    }
+
 }
